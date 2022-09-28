c2f5f9322cbbb81c57f325aa48240c62d6a47705
abilbotz1 <108676663+abilbotz1@users.noreply.github.com>
2022-09-28 19:31:21 +0800

    Update info-rules.js

--- plugins/info-rules.js
+++ plugins/info-rules.js
@@ -33,13 +33,13 @@ let rules = `➯ *Tɪᴅᴀᴋ Sᴘᴀᴍ Bᴏᴛ*
 └─┈⟅`
 let nth = `☰⟥⟝⟞⟝❨ *Rᴜʟᴇs Mʏ Bᴏᴛ* ❩⟞⟝⟞⟤☰`
 conn.send3ButtonImg(m.chat, `${pickRandom(flaaa2)}` + `${ucapan()} ` + `${name}`, nth, rules, 'Menu', '.menu', 'Owner', '.owner', 'Credit', '.credit', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: 'https://wa.me/p/5489299044451325/6281361281833',
+    mediaUrl: 'wa.me/6282146218274',
     mediaType: 2, 
     description: sgc,
-    title: "Sᴇᴡᴀ Cᴇᴋ Dɪsɪɴɪ Cᴜʏ!!!",
+    title: "Sᴇᴡᴀ ᴋʟɪᴋ ᴅɪsɪɴɪ Cᴜʏ!!",
     body: wm,
-    thumbnail: fs.readFileSync('./media/qr.png'),
-    sourceUrl: 'https://wa.me/p/5489299044451325/6281361281833',
+    thumbnail: fs.readFileSync('./media/qr.jpg'),
+    sourceUrl: 'wa.me/6282146218274',
      }}
   })
 }
